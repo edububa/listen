d11c4815d43296eee2ebf0c55b1a1bbf15520104
Luis Eduardo Bueso <luisedu@MacBook-Pro-de-Luis.local>
2016-01-17 12:05:08 +0100

    swift project started

--- swift/playList.swift
+++ swift/playList.swift
@@ -2,14 +2,11 @@
 import Foundation
 class Song {
     // src -> variable that contains the directory of the list
-    var src: String
+    let src: String
     // name -> variable that contains the name of the song
-    var name: String
+    let name: String
     // position -> variable that contains the position of a song
-    var position: Int {
-        get { return self.position }
-        set(newPosition) { self.position = newPosition }
-    }
+    var position: Int
     
     init(src: String, name: String) {
         self.src  = src
@@ -18,7 +15,7 @@ class Song {
     }
     
     func simpleDescription() -> String {
-        return "Track:  \(position)  Name: \(name)/n"
+        return "Track:  \(position)  Name: \(name)\n"
     }
 }
 
@@ -28,18 +25,23 @@ class List {
     // songs -> elements in the list
     var songs: [Song]
     
-    init(name: String) {
+    init(name: String, songs: [Song]) {
         self.name  = name
-        self.songs = [Song]()
+        self.songs = songs
     }
     
-    func addSong(song: Song) {}
+    func addSong(song: Song) {
+        let aux = [song]
+        self.songs += aux
+    }
+
+    func addSong(song: Song, pos: Int) {}
     
     func deleteSong(song: String?, pos: Int?) {}
     
     func simpleDescription() -> String {
-        var description = "Name of the list: \(name)/n"
-        for song in songs {
+        var description = "Name of the list: \(name)\n"
+        for song in self.songs {
             description += song.simpleDescription()
         }
         return description
@@ -47,7 +49,13 @@ class List {
 }
 
 // Start of script
-var s01 = Song(src: "H1CD1", name: "01..mp3")
+let s01 = Song(src: "H1CD1", name: "01..mp3")
+let s02 = Song(src: "H1CD1", name: "02..mp3")
+let s03 = Song(src: "H1CD1", name: "03..mp3")
+let s04 = Song(src: "H1CD1", name: "04..mp3")
 
+var U1 = List(name: "U1", songs: [s01, s02, s03])
 print(s01.simpleDescription())
- 
+print(U1.simpleDescription())
+U1.addSong(s04)
+print(U1.simpleDescription())
